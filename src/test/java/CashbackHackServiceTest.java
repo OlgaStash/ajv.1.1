e7f069277a867c9be32d06473f7647e2f348d32f
Olga Stashenyuk <olgastachenyk@mail.ru>
2021-11-24 21:30:43 +0300

    CI for junit4 please1

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -37,4 +37,15 @@ public class CashbackHackServiceTest {
 
     }
 
+    @Test
+    public void shouldTestRemainFrom500() {
+        CashbackHackService service = new CashbackHackService();
+        int payment = 500;
+
+        int actual = service.remain(payment);
+        int expected = 500;
+        assertEquals(actual, expected);
+
+    }
+
 }
