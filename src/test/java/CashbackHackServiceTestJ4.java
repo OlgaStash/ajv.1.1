d6296d9562dc3d5ff2d5fef713f5f62ae467799e
OlgaStash <77970381+OlgaStash@users.noreply.github.com>
2021-11-25 17:11:35 +0300

    Revert "Junit4"

--- src/test/java/CashbackHackServiceTestJ4.java
+++ src/test/java/CashbackHackServiceTestJ4.java
@@ -15,7 +15,6 @@ public class CashbackHackServiceTestJ4 {
 
     }
 
-
     @Test
     public void shouldTestRemainFrom1() {
         CashbackHackService service = new CashbackHackService();
@@ -27,7 +26,6 @@ public class CashbackHackServiceTestJ4 {
 
     }
 
-
     @Test
     public void shouldTestRemainFrom1000() {
         CashbackHackService service = new CashbackHackService();
@@ -35,18 +33,6 @@ public class CashbackHackServiceTestJ4 {
 
         int actual = service.remain(payment);
         int expected = 1000;
-        assertEquals(actual, expected);
-
-    }
-
-
-    @Test
-    public void shouldTestRemainFrom500() {
-        CashbackHackService service = new CashbackHackService();
-        int payment = 500;
-
-        int actual = service.remain(payment);
-        int expected = 500;
         assertEquals(actual, expected);
 
     }
